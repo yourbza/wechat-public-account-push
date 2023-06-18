90f6ecb888b41818624825ea398ebce4ab8219dc
yourbza <111673199+yourbza@users.noreply.github.com>
2023-06-18 22:50:32 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -39,13 +39,7 @@ const USER_CONFIG = {
           type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
         },
       ],
-      // 我们在一起已经有xxxx天了的配置
-      customizedDateList: [
-        // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
-      ],
+   
     },
   ],
 
