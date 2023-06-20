10e7856bf59e82d4d9f3d54ed47ba6e4062ba5e6
yourbza <111673199+yourbza@users.noreply.github.com>
2023-06-21 00:28:06 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ohEs56yER461n9MQUy2u4NW5ttSk',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '-TqcJbvnoo_9TwGMLCOx5J1QsVNl3TLIav_mpxEhcR4',
+      useTemplateId: 'AYP5-hHR9gTXsxkyYXj_LP7qFWA9Hoe8hdSl4Sa-TrM',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '06-01',
       festivals: [
